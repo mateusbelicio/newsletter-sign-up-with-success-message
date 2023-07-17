bf0b79f3ad68a849f2efaee9d7ed64b74a4e5b8c
Mateus Belicio <mateusbelicio@hotmail.com>
2023-07-17 08:19:56 -0300

    Feat: Success Message page structuring

--- src/pages/SuccessMessage/index.jsx
+++ src/pages/SuccessMessage/index.jsx
@@ -1,8 +1,12 @@
+import { Link } from 'react-router-dom';
+
 const SuccessMessage = () => {
 	return (
-		<div>
-      Success
-		</div>
+		<main>
+			<h1>Thanks for subscribing!</h1>
+			<p>A confirmation email has been sent to ash@loremcompany.com. Please open it and click the button inside to confirm your subscription.</p>
+			<Link to='/'>Dismiss message</Link>
+		</main>
 	);
 };
 
