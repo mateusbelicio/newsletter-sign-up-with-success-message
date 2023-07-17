202f72756e172256523c963993c3c1b94cf40846
Mateus Belicio <mateusbelicio@hotmail.com>
2023-07-16 23:10:37 -0300

    Feat: Home page structuring

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,33 @@
+import desktopImage from '../../assets/illustration-sign-up-desktop.svg';
+import mobileImage from '../../assets/illustration-sign-up-mobile.svg';
+import { device } from '../../styles/Theme';
+
 const Home = () => {
 	return (
-		<div>
-      Home
-		</div>
+		<main>
+			<picture>
+				<source media={device.tablet} srcSet={desktopImage} />
+				<img src={mobileImage} alt="Hero image" />
+			</picture>
+
+			<h1>Stay updated!</h1>
+			<p>Join 60,000+ product managers receiving monthly updates on:</p>
+
+			<ul>
+				<li>Product discovery and building what matters</li>
+				<li>Measuring to ensure updates are a success</li>
+				<li>And much more!</li>
+			</ul>
+
+			<form>
+				<div>
+					<label htmlFor="email">Email address</label>
+					<input type="email" name="email" id="in-email" />
+					<span>Valid email required</span>
+				</div>
+				<button type="submit">Subscribe to monthly newsletter</button>
+			</form>
+		</main>
 	);
 };
 
